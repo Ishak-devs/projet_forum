df47fc5dafcaa0fa769f1e6b7241d1183ee7ac7e
isk452 <kouicicontact@yahoo.com>
2025-02-28 15:17:56 +0100

    commit du 28/02 le main est ok

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba3ffc4bf0183b9f27e096d1982603953bff50b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a95a918c7b1d61615b64ec6e78775fb37296116")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
