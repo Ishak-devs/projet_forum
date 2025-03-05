3ab10c182a4abd41fb714d8fd59ee55103ca7679
isk452 <kouicicontact@yahoo.com>
2025-03-05 10:58:30 +0100

    commit probleme resolu

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a95a918c7b1d61615b64ec6e78775fb37296116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1428d9dacb8633428beaa9f523f7f21b53a7970d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
