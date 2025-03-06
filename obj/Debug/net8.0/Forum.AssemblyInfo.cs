fba0c58e7af00f772ba38a14214ff1a0aba58dc2
isk452 <kouicicontact@yahoo.com>
2025-03-06 15:47:10 +0100

    commit resolution bug les eleves s ajoutent tous !

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872271912277f21c32eceb85a466cb8b9dd20544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a6bdd90f6177f0bc19e224e3c9a2b79b553698")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
