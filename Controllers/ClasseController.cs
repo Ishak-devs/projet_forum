ccbb006cbccccb0a7e373ef0b7438f43b1bd2d7e
isk452 <kouicicontact@yahoo.com>
2025-03-06 15:47:10 +0100

    commit resolution bug les eleves s ajoutent tous !

--- Controllers/ClasseController.cs
+++ Controllers/ClasseController.cs
@@ -29,9 +29,11 @@ namespace Forum.Controllers
          
             .ToList();  //Méthode tolist pour convertir un tableau en liste
 
+
             var model = new ClasseViewModel
             {
-                Eleves = new SelectList(Eleves, "Value", "Text")
+                Eleves = new SelectList(Eleves, "Value", "Text"),
+                Eleveschoisis = new List<int>()
             };
 
             return View(model);
@@ -58,6 +60,7 @@ namespace Forum.Controllers
 
                     ViewBag.Message_eleve = "Elève ajouté !";
                 }
+
                 if (!classeview.Eleveschoisis.Contains(classeview.id_eleve.Value)) //Vérification de l'existence de l'id dans la liste afin d'éviter les doublons.
                 {
                     classeview.Eleveschoisis.Add(classeview.id_eleve.Value); //Ajouter un élève à Eleveschoisis grace à la méthode add.
