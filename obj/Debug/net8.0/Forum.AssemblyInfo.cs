459d6634364131b8a1e0c8d40a5a5286d87d026b
isk452 <kouicicontact@yahoo.com>
2025-03-06 16:40:37 +0100

    ok

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602646d26b66c13bf92f60c3540e4685f959128d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65fc88d100385c439b0b5b31b0530d8013d0934b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
