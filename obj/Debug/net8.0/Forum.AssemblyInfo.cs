3f9b9c070d591037a75b2c0bf44d9271566f7458
isk452 <kouicicontact@yahoo.com>
2025-03-10 12:20:30 +0100

    ajouter un eleve a la table fonctionne :

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe48b013acb0109f2bddbb66707023dbe0999a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73c4d54129422377639583d130113b15cc74cb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
