bc277148eddb536727d4fe632f0bbeb33cbaedd1
Ishak Koici <i.kouici@insta.fr>
2025-03-13 10:46:24 +0100

    affichage ok

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728bef19daf3eae78ed090272a40a43c574dcc74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3005c01917a25e2af4c5a512acd8037e16fa9e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
