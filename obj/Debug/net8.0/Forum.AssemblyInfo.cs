ef9e60c87bde6d11f1b71ce3004b1a38ad257d61
Ishak Koici <i.kouici@insta.fr>
2025-03-13 11:03:45 +0100

    mend

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc277148eddb536727d4fe632f0bbeb33cbaedd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cad068eab462ef734139ed2ce03bb19d8ceedcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/signup.js
@@ -0,0 +1,26 @@
+﻿ //Fonction affichant les matières si l'inscri est un professeur
+    function verif_role() {
+        const role_saisie = document.querySelector('input[name="Role"]:checked').value;
+    const Matiere_saisie = document.getElementById("Matiere_saisie");
+    // const Autre_matiere = document.getElementById("Autre_matiere");
+
+    if (role_saisie === 'Professeur') {
+        Matiere_saisie.style.display = 'block';
+                } else {
+        Matiere_saisie.style.display = 'none';
+                    // Autre_matiere.style.disaplay = 'none';
+                         
+                }
+
+            }
+
+    function verif_autre_matiere() {
+                          const Matiere_saisie = document.getElementById("Matiere_saisie");
+    const Autre_matiere = document.getElementById("Autre_matiere");
+
+    if (Matiere_saisie.value === 'Autre') {
+        Autre_matiere_input.style.display = 'block';
+                       } else {
+        Autre_matiere_input.style.display = 'none';
+                       }
+                    }
