4cf43eca224bb680746881ab666dd50d70e0caff
ishakdév <kouicicontact@yahoo.com>
2025-03-17 12:10:15 +0100

    probleme pour centrer le form verticalement

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a368d4381630368002d93d87afa7c2e1ef72a90a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96499b49a928b9454ddff2390faecf9f4bfdb203")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
