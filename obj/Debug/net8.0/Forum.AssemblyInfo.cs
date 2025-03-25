8d85e8adff8008af271578f0de6107359ba7cd5a
ishak <kouicicontact@yahoo.com>
2025-03-25 09:42:37 +0100

    debut de l epic forum

--- obj/Debug/net8.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d943cadc402adc42a7537546e815f1339eaaff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be5b6340d659580c4f3f8126b6539f855072d439")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
