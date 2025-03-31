11ec99ac77840389c610608ee73aebf017afc2b3
Rudi Herouard <rudi.herouard@gmail.com>
2025-03-31 14:08:51 +0200

    Fix autre matière issue

--- wwwroot/js/signup.js
+++ wwwroot/js/signup.js
@@ -1,26 +1,28 @@
-﻿ //Fonction affichant les matières si l'inscri est un professeur
-    function verif_role() {
-        const role_saisie = document.querySelector('input[name="Role"]:checked').value;
-    const Matiere_saisie = document.getElementById("Matiere_saisie");
-    // const Autre_matiere = document.getElementById("Autre_matiere");
+﻿//Fonction affichant les matières si l'inscri est un professeur
+const Matiere = document.getElementById("Matiere");
+const Matiere_saisie = document.getElementById("Matiere_saisie");
+const Autre_matiere_input = document.getElementById("Autre_matiere_input");
 
+function verif_role() {
+    const role_saisie = document.querySelector('input[name="Role"]:checked').value;
+    
     if (role_saisie === 'Professeur') {
-        Matiere_saisie.style.display = 'block';
-                } else {
-        Matiere_saisie.style.display = 'none';
-                    // Autre_matiere.style.disaplay = 'none';
-                         
-                }
+        Matiere.style.display = 'block';
+    } 
+    else {
+        Matiere.style.display = 'none';
+    }
+    
+     verif_autre_matiere()
+}
 
-            }
-
-    function verif_autre_matiere() {
-                          const Matiere_saisie = document.getElementById("Matiere_saisie");
-    const Autre_matiere = document.getElementById("Autre_matiere");
-
-    if (Matiere_saisie.value === 'Autre') {
+function verif_autre_matiere() {
+    const role_saisie = document.querySelector('input[name="Role"]:checked').value;
+    
+    if (Matiere_saisie.value === 'Autre' && role_saisie === 'Professeur') {
         Autre_matiere_input.style.display = 'block';
-                       } else {
+    } 
+    else {
         Autre_matiere_input.style.display = 'none';
-                       }
-                    }
+    }
+}
